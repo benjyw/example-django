24bdff54bbdd11909bbef0664818b85bef72763d
Benjy Weinberger <benjyw@gmail.com>
2021-05-17 18:08:31 -0700

    WIP

--- helloworld/ui/views.py
+++ helloworld/ui/views.py
@@ -7,15 +7,22 @@ from django.http import Http404, HttpResponse
 from helloworld.util.backend import get_backend_url
 
 
-def index(request) -> HttpResponse:
-    person_slug = request.GET.get("name", "")
-    lang = request.GET.get("lang", "en")
-    person_url_prefix = get_backend_url("helloworld.service.user")
-    person_url = f"{person_url_prefix}/person/{person_slug}"
-    response = requests.get(person_url)
+def query_backend(url: str) -> str:
+    response = requests.get(url)
     if response.status_code == 404:
-        raise Http404(f"Backend error: {person_url}")
+        raise Http404(f"Backend error: {url}")
     # All other backend errors should become 500 errors for the frontend.
     response.raise_for_status()
-    name = response.text
-    return HttpResponse(f"RENDER FOR: {name} in {lang}")
+    return response.text
+
+
+def index(request) -> HttpResponse:
+    person_slug = request.GET.get("person", "")
+    lang = request.GET.get("lang", "en")
+
+    # Get person's full name.
+    name = query_backend(f"{get_backend_url('helloworld.service.user')}/person/{person_slug}")
+
+    # Get greeting.
+    greeting = query_backend(f"{get_backend_url('helloworld.service.welcome')}/translate/hello/{lang}")
+    return HttpResponse(f"RENDER FOR: {greeting} {name}")
