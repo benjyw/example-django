bbd444ac1f6735d3b2b93fe4d67129b7b5f158e7
Benjy Weinberger <benjyw@gmail.com>
2021-05-18 17:45:05 -0700

    WIP

--- helloworld/translate/conftest.py
+++ helloworld/translate/conftest.py
@@ -1,21 +1,7 @@
 # Copyright 2021 Pants project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import os
-from tempfile import mkdtemp
-
-from django.conf import settings
+from helloworld.util.settings_for_tests import configure_settings
 
 
-def pytest_configure():
-    settings.configure(
-        TIME_ZONE="UTC",
-        USE_TZ=True,
-        INSTALLED_APPS=["helloworld.greet", "helloworld.translate"],
-        DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-                "NAME": os.path.join(mkdtemp(), "test.sqlite3"),
-            }
-        },
-    )
+configure_settings(["helloworld.greet", "helloworld.translate"])
